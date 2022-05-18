537a87968b1f1518e6efeab3fd54082399094f80
guifuentess <89105754+guifuentess@users.noreply.github.com>
2022-05-18 17:27:44 -0300

    criei ex006

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`a velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) {
+    console.log('Você ultapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Braleiro!')
+} else {
+    console.log('Estrangeiro!')
+}
