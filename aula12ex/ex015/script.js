fa229b87994ff28899762329cc52c495a8946ba0
guifuentess <89105754+guifuentess@users.noreply.github.com>
2022-08-23 11:30:33 -0300

    att

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,7 +1,8 @@
-function verificar() {
+function Verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
     if (fano.value.lenght == 0 ||  fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
