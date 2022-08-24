9f51c553c63d1f06506643649c5ce57c40af958b
guifuentess <89105754+guifuentess@users.noreply.github.com>
2022-08-23 21:00:36 -0300

    exercicios

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,10 +3,48 @@ function Verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.lenght == 0 ||  fano.value > ano) {
+    if (fano.value.lenght == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        window.alert('tudo ok')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade <10) {
+                //CRIANÇA
+                img.setAttribute('src', 'bebehomem.png')
+            } else if (idade < 21) {
+                //JOVEM
+                img.setAttribute('src', 'jovemhomem.png')
+            } else if (idade < 50) {
+                //ADULTO
+                img.setAttribute('src', 'adultohomem.png')
+            } else {
+                //IDOSO
+                img.setAttribute('src', 'idosohomem.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade <10) {
+                //CRIANÇA
+                img.setAttribute('src', 'bebemulher.png')
+            } else if (idade < 21) {
+                //JOVEM
+                img.setAttribute('src', 'jovemmulher.png')
+            } else if (idade < 50) {
+                //ADULTO
+                img.setAttribute('src', 'adultamulher.png')
+            } else {
+                //IDOSO
+                img.setAttribute('src', 'idosamulher.png')
+            }
+        }
+        res.style.textAling = 'center'
+        res.innerHTML = `Destectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
     
 }
