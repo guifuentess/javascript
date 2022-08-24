98c2e9ca14858b59dee027233dafb19974ad10c7
guifuentess <89105754+guifuentess@users.noreply.github.com>
2022-08-23 22:20:11 -0300

    exercicios

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,21 +5,26 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert ('[ERRO] Complete todos os campos para prosseguir')
+        res.innerHTML = 'Impossivel contar!'
+        //window.alert('[ERRO] Faltam dados!!')
     } else {
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-        if ( i < f) {
+        if  (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
             //contagem crescente
             for(let c = i; c <= f; c += p) {
                 res.innerHTML += ` ${c} \u{1F449}`
             }
-        }else {
+        } else {
             //contagem regressiva
-            for(let c = i; c >= f; c -= 0) {
-                res.innerHTML += ` ${c} \u{1F499}`
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
             }
         }
         res.innerHTML += `\u{1F3C1}`
